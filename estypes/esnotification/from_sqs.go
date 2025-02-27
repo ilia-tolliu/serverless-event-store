be318a1d72e773fc50645e55d74f871808d914a3
ilia-tolliu <ilia.tolliu@gmail.com>
2025-02-27 16:38:08 +0100

    feature: add EsNotification converter from SQS message
    Suggested way of using notifications is to create an SQS queue and subscribe it to the Event Store SNS topic.
    
    This converter extracts EsNotification payload from SQS message.

---
+++ estypes/esnotification/from_sqs.go
@@ -0,0 +1,27 @@
+package esnotification
+
+import (
+	"encoding/json"
+	"fmt"
+	sqstypes "github.com/aws/aws-sdk-go-v2/service/sqs/types"
+)
+
+func NewFromSqsMessage(sqsMessage sqstypes.Message) (EsNotification, error) {
+	var snsMessage snsNotification
+	err := json.Unmarshal([]byte(*sqsMessage.Body), &snsMessage)
+	if err != nil {
+		return EsNotification{}, fmt.Errorf("failed to unmarshal snsNotification: %w", err)
+	}
+
+	var esNotification EsNotification
+	err = json.Unmarshal([]byte(snsMessage.Message), &esNotification)
+	if err != nil {
+		return EsNotification{}, fmt.Errorf("failed to unmarshal EsNotification: %v", err)
+	}
+
+	return esNotification, nil
+}
+
+type snsNotification struct {
+	Message string `json:"Message,omitempty"`
+}
--- estypes/esnotification/notification.go
+++ estypes/esnotification/notification.go
@@ -1,4 +1,4 @@
-package estypes
+package esnotification
 
 import (
 	"github.com/google/uuid"
